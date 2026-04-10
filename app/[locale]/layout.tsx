77ca85f2f0a3f3d0c14e0df210ab75c1ce8655fd
Donckers Florian <fdonckersf@gmail.com>
2026-04-10 09:20:58 +0200

    new name for onglet

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,43 +1,43 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { NextIntlClientProvider, hasLocale } from 'next-intl';
-import { notFound } from 'next/navigation';
-import { routing } from '@/i18n/routing';
+import type { Metadata } from "next";
+import "./globals.css";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
 
 export const metadata: Metadata = {
-	title: 'New App',
-	description: 'Template par défaut Prisma et NextIntl',
+  title: "HE2B",
+  description: "Jeu de guitare héros pour les étudiants de HE2B",
 };
 
 export default async function RootLayout({
-	children,
-	params,
+  children,
+  params,
 }: Readonly<{
-	children: React.ReactNode;
-	params: Promise<{ locale: string }>;
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }>) {
-	const { locale } = await params;
-	if (!hasLocale(routing.locales, locale)) {
-		notFound();
-	}
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
 
-	// Charge les messages de traduction pour la locale demandée.
-	let messages: Record<string, unknown> | undefined;
-	try {
-		// Le chemin relatif depuis `app/[locale]/layout.tsx` vers `messages/{locale}.json`
-		messages = (await import(`../../messages/${locale}.json`)).default;
-	} catch {
-		// Si les messages ne sont pas trouvés, afficher 404 (ou gérer autrement)
-		notFound();
-	}
+  // Charge les messages de traduction pour la locale demandée.
+  let messages: Record<string, unknown> | undefined;
+  try {
+    // Le chemin relatif depuis `app/[locale]/layout.tsx` vers `messages/{locale}.json`
+    messages = (await import(`../../messages/${locale}.json`)).default;
+  } catch {
+    // Si les messages ne sont pas trouvés, afficher 404 (ou gérer autrement)
+    notFound();
+  }
 
-	return (
-		<html lang={locale}>
-			<body className={`antialiased`}>
-				<NextIntlClientProvider locale={locale} messages={messages}>
-					{children}
-				</NextIntlClientProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang={locale}>
+      <body className={`antialiased`}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
